d5501e88be43b1313290f525278415e42d333c07
adonis30 <46707640+adonis30@users.noreply.github.com>
2025-05-12 18:05:34 +0200

    Update company.model.ts
    mm

--- lib/database/models/company.model.ts
+++ lib/database/models/company.model.ts
@@ -57,7 +57,7 @@ const CompanySchema = new Schema({
   url: { type: String },
   categories: [{ type: Schema.Types.ObjectId, ref: 'Category' }],
   sector: [{ type: Schema.Types.ObjectId, ref: 'Sector' }],
- investmentAsk: { type: mongoose.Schema.Types.ObjectId, ref: "Investmentask" },
+  investmentAsk: { type: Schema.Types.ObjectId, ref: 'InvestmentAsk' },
   people: [{ type: Schema.Types.ObjectId, ref: 'Person' }],
   fundedBy: [{ type: Schema.Types.ObjectId, ref: 'FundingSource' }],
   fundingTypes: [{ type: Schema.Types.ObjectId, ref: 'FundingType' }],
