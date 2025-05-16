66e70e8e93bd5d482d717674ceeca8bd8b5c0350
adonis30 <46707640+adonis30@users.noreply.github.com>
2025-05-16 07:39:25 +0200

    Update news.ts

--- app/api/news.ts
+++ app/api/news.ts
@@ -7,9 +7,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   const url = `https://newsapi.org/v2/top-headlines?category=business&q=${investor}&apiKey=${process.env.NEWS_API_KEY}`;
 
   try {
-    const response = await fetch(url);
+    const response = await fetch(url); 
     const data = await response.json();
-
+    console.log("data", data)
     if (!data.articles || data.articles.length === 0) {
       return res.status(404).json({ error: 'No news articles found.' });
     }
