1aa6799a96616ddc1dd5c2a4b91400a288b9c146
adonis30 <46707640+adonis30@users.noreply.github.com>
2025-05-31 23:16:09 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import "keen-slider/keen-slider.min.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Providers from "./providers";
 
