d2053d67f275f5c5b2214b2101e59ba874c425e1
adonis30 <46707640+adonis30@users.noreply.github.com>
2025-06-23 12:05:04 +0200

    Add files via upload

---
+++ utils/formatUSD.ts
@@ -0,0 +1,6 @@
+export const formatUSD = (num: number): string => {
+  if (num >= 1e9) return (num / 1e9).toFixed(1) + "B";
+  if (num >= 1e6) return (num / 1e6).toFixed(1) + "M";
+  if (num >= 1e3) return (num / 1e3).toFixed(1) + "K";
+  return num.toString();
+};
