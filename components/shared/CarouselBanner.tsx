631ed92bfb47e6bba9fdb10c43c7378d55c7ebd5
adonis30 <46707640+adonis30@users.noreply.github.com>
2025-07-10 01:20:45 +0200

    Update CarouselBanner.tsx

--- components/shared/CarouselBanner.tsx
+++ components/shared/CarouselBanner.tsx
@@ -30,7 +30,7 @@ export default function CarouselBanner() {
           {slides.map((slide, index) => (
              <div
               key={index}
-              className="min-w-full h-[74vh] relative flex items-end justify-center"
+              className="min-w-full h-[70vh] relative flex items-end justify-center"
             >
               <Image
   src={slide.image}
