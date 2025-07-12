4ea74f0884db5a1c87ba9495f218a31f4399fbc0
adonis30 <46707640+adonis30@users.noreply.github.com>
2025-07-12 16:24:07 +0200

    Update MobileNav.tsx

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -27,7 +27,7 @@ const MobileNav = () => {
         </SheetTrigger>
             <SheetContent className="flex flex-col gap-6 bg-white md:hidden">
               <Image 
-               src="/assets/images/logo.svg"
+               src="/assets/images/logo3.svg"
                alt="logo"
                width={128}
                height={38}
